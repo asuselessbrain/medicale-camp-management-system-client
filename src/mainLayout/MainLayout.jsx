a7ba1ccaee9ad5ba6ff9c481a2394badc68099bd
asuselessbrain <shovon1023456789@gmail.com>
2024-09-11 22:37:09 +0600

    scroll to top when change route using react router dom in added

--- src/mainLayout/MainLayout.jsx
+++ src/mainLayout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 const MainLayout = () => {
     return (
         <div className="max-w-[1440px] mx-auto">
             <NavBar />
+            <ScrollToTop />
             <Outlet />
         </div>
     );
