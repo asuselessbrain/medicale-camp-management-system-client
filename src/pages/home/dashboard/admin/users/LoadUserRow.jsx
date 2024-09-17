296a051ae79e77b0011860c7f1773e196a4fd09c
asuselessbrain <shovon1023456789@gmail.com>
2024-09-17 22:13:47 +0600

    update user role modal design updated

--- src/pages/home/dashboard/admin/users/LoadUserRow.jsx
+++ src/pages/home/dashboard/admin/users/LoadUserRow.jsx
@@ -36,7 +36,10 @@ const LoadUserRow = ({ user, index }) => {
           {new Date(lastLoginTime).toLocaleString()}
         </td>
         <td className="px-6 py-4 text-right flex items-center gap-2">
-          <button onClick={handleToggle} className="flex p-2.5 bg-yellow-500 rounded-xl hover:rounded-3xl hover:bg-yellow-600 transition-all duration-300 text-white">
+          <button
+            onClick={handleToggle}
+            className="flex p-2.5 bg-yellow-500 rounded-xl hover:rounded-3xl hover:bg-yellow-600 transition-all duration-300 text-white"
+          >
             <LiaEditSolid size={24} />
           </button>
 
@@ -49,13 +52,13 @@ const LoadUserRow = ({ user, index }) => {
               aria-hidden="true"
               className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
             >
-              <div className="relative p-4 w-full max-w-2xl max-h-full">
+              <div className="relative p-4 w-full max-w-md max-h-full">
                 {/* <!-- Modal content --> */}
                 <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                   {/* <!-- Modal header --> */}
                   <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                     <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                      Static modal
+                      Update Role
                     </h3>
                     <button
                       type="button"
@@ -69,30 +72,34 @@ const LoadUserRow = ({ user, index }) => {
                   </div>
                   {/* <!-- Modal body --> */}
                   <div className="p-4 md:p-5 space-y-4">
-                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                      With less than a month to go before the European Union
-                      enacts new consumer privacy laws for its citizens,
-                      companies around the world are updating their terms of
-                      service agreements to comply.
-                    </p>
-                    <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                      The European Union’s General Data Protection Regulation
-                      (G.D.P.R.) goes into effect on May 25 and is meant to
-                      ensure a common set of data rights in the European Union.
-                      It requires organizations to notify users as soon as
-                      possible of high-risk data breaches that could personally
-                      affect them.
-                    </p>
+                    <form className="max-w-sm mx-auto">
+                      <label
+                        htmlFor="countries"
+                        className="block mb-4 text-sm font-medium text-gray-900 dark:text-white text-left"
+                      >
+                        Select a role
+                      </label>
+                      <select
+                        id="countries"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      >
+                        <option selected>Select a role</option>
+                        <option value="US">User</option>
+                        <option value="CA">Organizer</option>
+                        <option value="FR">Doctor</option>
+                        <option value="DE">Admin</option>
+                      </select>
+                    </form>
                   </div>
                   {/* <!-- Modal footer --> */}
-                  <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                  <div className="flex items-center p-4 md:p-5">
                     <button
                       data-modal-hide="static-modal"
                       onClick={handleToggle}
                       type="button"
                       className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
-                      I accept
+                      Update
                     </button>
                     <button
                       data-modal-hide="static-modal"
@@ -100,7 +107,7 @@ const LoadUserRow = ({ user, index }) => {
                       type="button"
                       className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                     >
-                      Decline
+                      Cancel
                     </button>
                   </div>
                 </div>
