30072b3df6779ca35d7c4c674813be7e0c59c494
asuselessbrain <shovon1023456789@gmail.com>
2024-09-24 22:03:50 +0600

    show my added product table design completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@
 // import LoadUser from "./dashboard/admin/users/LoadUser";
 import CampCard from "../../components/campCard/CampCard";
 import AddCamp from "./dashboard/organizer/addCamp/AddCamp";
+import MyAddedCamp from "./dashboard/organizer/myAddedCamp/MyAddedCamp";
 import Slider from "./slider/Slider";
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       {/* <LoadUser /> */}
       <AddCamp />
       <CampCard />
+      <MyAddedCamp />
     </div>
   );
 };
