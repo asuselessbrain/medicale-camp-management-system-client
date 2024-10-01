b003c3e932580c620708c24c8bed8cb331e597f0
asuselessbrain <shovon1023456789@gmail.com>
2024-10-01 19:40:08 +0600

    get data for individual camp added

--- src/pages/campDetail/CampDetails.jsx
+++ src/pages/campDetail/CampDetails.jsx
@@ -1,7 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import useAxiosProtected from "../../hooks/useAxiosProtected";
+import Spinner from "../../components/spinner/Spinner";
 
 const CampDetails = () => {
   const { id } = useParams();
+  const axiosProtected = useAxiosProtected();
+
+  const { data: campData = [], isPending } = useQuery({
+    queryKey: ["campData"],
+    queryFn: async () => {
+      const { data } = await axiosProtected(`/view-camp-details/${id}`);
+      return data;
+    },
+  });
+
+  if (isPending) {
+    return <Spinner />;
+  }
 
   return <div></div>;
 };
