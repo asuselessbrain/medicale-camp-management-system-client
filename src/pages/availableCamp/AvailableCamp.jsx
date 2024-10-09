b6989059f00052239489a2bae2421f971f5418ee
asuselessbrain <arfan18@cse.pstu.ac.bd>
2024-10-09 19:54:54 +0600

    available camp page design completed

--- src/pages/availableCamp/AvailableCamp.jsx
+++ src/pages/availableCamp/AvailableCamp.jsx
@@ -1,11 +1,34 @@
-
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import Title from "../../components/shared/Title";
+import CampCard from "../../components/campCard/CampCard";
+import Spinner from "../../components/spinner/Spinner";
 
 const AvailableCamp = () => {
-    return (
-        <div>
-            This is avilable camp page
-        </div>
-    );
+  const axiosPublic = useAxiosPublic();
+
+  const { data: availableCamp = [], isLoading } = useQuery({
+    queryKey: ["availableCamp"],
+    queryFn: async () => {
+      const { data } = await axiosPublic("/all-camp");
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div>
+      <Title title="Available Camp" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {availableCamp.map((popularCamp) => (
+          <CampCard key={popularCamp._id} popularCamp={popularCamp} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AvailableCamp;
