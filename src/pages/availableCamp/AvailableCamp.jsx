58b4a363b27002a4a813c5cfdee577ecaec15442
asuselessbrain <arfan18@cse.pstu.ac.bd>
2024-10-10 00:38:11 +0600

    searching functionality completed

--- src/pages/availableCamp/AvailableCamp.jsx
+++ src/pages/availableCamp/AvailableCamp.jsx
@@ -3,30 +3,46 @@ import useAxiosPublic from "../../hooks/useAxiosPublic";
 import Title from "../../components/shared/Title";
 import CampCard from "../../components/campCard/CampCard";
 import Spinner from "../../components/spinner/Spinner";
+import PaginationDesign from "../../components/peginationDesign/PaginationDesign";
+import SearchBar from "../../components/searchBar/SearchBar";
+import { useState } from "react";
 
 const AvailableCamp = () => {
   const axiosPublic = useAxiosPublic();
+  const [search, setSearch] = useState("")
 
-  const { data: availableCamp = [], isLoading } = useQuery({
-    queryKey: ["availableCamp"],
+  const { data: availableCamp = [], isLoading, isFetching } = useQuery({
+    queryKey: ["availableCamp", search],
     queryFn: async () => {
-      const { data } = await axiosPublic("/all-camp");
+      const { data } = await axiosPublic(`/all-camp?search=${search}`);
       return data;
     },
   });
 
-  if (isLoading) {
+  if (isFetching) {
     return <Spinner />;
   }
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    const searchResult = e.target.search_bar.value;
+    setSearch(searchResult)
+    console.log(searchResult)
+
+  };
+
   return (
-    <div>
+    <div className="p-4">
       <Title title="Available Camp" />
+      <SearchBar handleSearch={handleSearch} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {availableCamp.map((popularCamp) => (
           <CampCard key={popularCamp._id} popularCamp={popularCamp} />
         ))}
       </div>
+
+      <PaginationDesign />
     </div>
   );
 };
